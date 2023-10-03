b88c31e0791124788ce6c8813b27468b0f4b54af
IgbeAjaga <ajagaigbe144@gmail.com>
2023-10-03 21:26:53 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python3
-"""
-Flask server (variable app)
-"""
+'''
+Create Flask app and register the blueprint app_views to the Flask instance app.
+'''
 
-from flask import Flask, Blueprint, make_response, jsonify
-from models import storage
 from os import getenv
-from api.v1.views import app_views
+from flask import Flask, jsonify
 from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
-# Create a CORS instance and configure it
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+# enable CORS and allow for origins:
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """
-    Teardown app context.
-
-    This function is called when the Flask app context is torn down.
-    It ensures that the database connection is closed properly.
-    """
-    storage.close()
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """
-    Handle 404 errors by returning a JSON response.
-
-    This function is used as an error handler for 404 (Not Found) errors.
-    It returns a JSON response with an error message when a route is not found.
-    """
-    return jsonify({"error": "Not found"}), 404
-
-# Register the blueprint
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+
+@app.teardown_appcontext
+def teardown_engine(exception):
+    '''
+    Removes the current SQLAlchemy Session object after each request.
+    '''
+    storage.close()
+
+
+# Error handlers for expected app behavior:
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    Error handler for 404 - Not Found.
+    Returns an error message in JSON format.
+    '''
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
+
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
