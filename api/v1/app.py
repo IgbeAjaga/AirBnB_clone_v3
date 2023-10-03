f2ef5af4b6e2df29e0dea57391eee233fec3b5a0
IgbeAjaga <ajagaigbe144@gmail.com>
2023-10-03 12:18:26 +0000

    doc update

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,14 +17,20 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """
-    Teardown app context
+    Teardown app context.
+
+    This function is called when the Flask app context is torn down.
+    It ensures that the database connection is closed properly.
     """
     storage.close()
 
 @app.errorhandler(404)
 def page_not_found(e):
     """
-    Handle 404 errors by returning a JSON response
+    Handle 404 errors by returning a JSON response.
+
+    This function is used as an error handler for 404 (Not Found) errors.
+    It returns a JSON response with an error message when a route is not found.
     """
     return jsonify({"error": "Not found"}), 404
 
@@ -33,6 +39,6 @@ app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
 if __name__ == "__main__":
-host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,11 +6,19 @@ API status and statistics.
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """
     Return the status of your API.
+
+    This endpoint returns a JSON response indicating that the API
     """
     return jsonify({"status": "OK"})
 
@@ -18,6 +26,9 @@ def api_status():
 def api_stats():
     """
     Retrieve the number of each object by type.
+
+    This endpoint retrieves and returns the count of each object type
+    (e.g., amenities, cities, places) in the storage.
     """
     classes = {
         "amenities": storage.count(Amenity),
